d5153fa3f9d05d61a45c75a0b18b42561301de29
shayanxmoradi <shayan.moradi92@gmail.com>
2024-06-22 10:26:24 +0200

    merge

--- first_eclipse_proj/src/first_eclipse_proj/Main.java
+++ first_eclipse_proj/src/first_eclipse_proj/Main.java
@@ -3,5 +3,8 @@ package first_eclipse_proj;
 public class Main {
 	 public static void main(String[] args) {	
 		System.out.print("hi");
+		System.out.print("byte");
+		
+		
 }
 }
--- first_eclipse_proj/src/first_eclipse_proj/Person.java
+++ first_eclipse_proj/src/first_eclipse_proj/Person.java
@@ -1,5 +1,8 @@
 package first_eclipse_proj;
 
 public class Person {
+	void byer() {
+	System.out.print("byte");
 
+}
 }
